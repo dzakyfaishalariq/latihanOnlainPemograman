55b7a5dce531e08a38b0a71143a6bc47d8de89ad
DzakyFaishalariq <84628553+dzakyfaishalariq@users.noreply.github.com>
2022-01-22 20:55:22 +0700

    Membuat pogram penghitung baris aritmatika

--- membuat_segitiga_penjumlahan.py
+++ membuat_segitiga_penjumlahan.py
@@ -1,2 +1,29 @@
-print("Hallo Apa kabar")
-# ini code
+"""
+    Pembuatan segitiga dengan basis hitungan yang ada dalam python
+    dengan menurut seperti barisan aritmatika     
+"""
+from colorama import Fore, Back, Style
+import os
+os.system('cls')
+# membuat fungsi
+
+
+def segitiga_hitung(tinggi):
+    print(Fore.YELLOW)
+    for i in range(tinggi):
+        hasil = 0
+        for j in range(i+1):
+            if j < i:
+                print(j+1, end='+')
+            else:
+                print(j+1, end=' ')
+            hasil += j+1
+        print("{:>10}{}".format('=', hasil))
+    print(Style.RESET_ALL)
+
+
+print(Fore.MAGENTA)
+tinggi = int(input('Masukan angka : '))
+print(Style.RESET_ALL)
+print("="*20)
+segitiga_hitung(tinggi)
